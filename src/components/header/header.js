3c13ba827a5f087772270c14c940ad87905c4b96
Shoaib <shoaibzkhalid@gmail.com>
2024-02-10 20:43:08 +0500

    updated style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ import { ProfileType } from "../../types"
 import SocialLinks from "../social-links/social-links"
 
 const Header = ({ initials, social }) => (
-  <header className="flex justify-between p-4 lg:px-8">
+  <header className="flex justify-between p-4 lg:px-8 items-center">
 
     <span className="inline-flex w-14 h-14 lg:mt-4 font-header font-bold text-xl justify-center items-center text-center text-front border-2 border-solid border-front rounded-full">
       {initials}
